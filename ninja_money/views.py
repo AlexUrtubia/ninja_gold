fe531c658076f3643f3a448424843d381468c7fd
Alex Urtubia <alex.urtubia.contreras@gmail.com>
2021-08-27 21:34:38 -0400

    3er commit

--- ninja_money/views.py
+++ ninja_money/views.py
@@ -1,22 +1,34 @@
 from django.shortcuts import redirect, render
 from random import randint
+from datetime import datetime
+
 # Create your views here.
 def inicio(request):
     if 'gold_amount' not in request.session:
         request.session['gold_amount'] = 0
+        request.session['jugadas'] = []
     return render(request, 'index.html')
 
 def process_money(request):
+    time = datetime.now().strftime("a las %H:%M %p del %d-%m-%Y")
     if "farm" in request.POST:
-        request.session['gold_amount'] += randint(10,20)
+        farm_prize = randint(10,20)
+        request.session['gold_amount'] += farm_prize
+        request.session['jugadas'].append([farm_prize, "Farm", time])
         
     if "cave" in request.POST:
-        request.session['gold_amount'] += randint(5,10)
+        cave_prize = randint(5,10)
+        request.session['gold_amount'] += cave_prize
+        request.session['jugadas'].append([cave_prize, "Cave", time])
         
     if "house" in request.POST:
-        request.session['gold_amount'] += randint(10,15)
+        house_prize = randint(10,15)
+        request.session['gold_amount'] += house_prize
+        request.session['jugadas'].append([house_prize, "House", time])
         
     if "casino" in request.POST:
-        request.session['gold_amount'] += randint(-50,50)
+        casino_prize = randint(-50,50)
+        request.session['gold_amount'] += casino_prize
+        request.session['jugadas'].append([casino_prize, "Casino", time])
         
     return redirect('/')
